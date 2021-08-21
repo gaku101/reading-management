c10ed8dbc74c6482f942b7d4f097964e78eaca13
takehiro <takehiro-r133@hotmail.co.jp>
2021-08-21 19:18:59 +0900

    add users table #17

--- db/migration/000001_init_schema.up.sql
+++ db/migration/000001_init_schema.up.sql
@@ -8,7 +8,6 @@ CREATE TABLE "users" (
   "password_changed_at" timestamptz NOT NULL DEFAULT '0001-01-01 00:00:00Z',
   "created_at" timestamptz NOT NULL DEFAULT (now())
 );
-
 CREATE TABLE "posts" (
   "id" bigserial PRIMARY KEY,
   "author" varchar NOT NULL,
@@ -17,51 +16,43 @@ CREATE TABLE "posts" (
   "created_at" timestamptz NOT NULL DEFAULT (now()),
   "updated_at" timestamptz NOT NULL DEFAULT '0001-01-01 00:00:00Z'
 );
-
 CREATE TABLE "comments" (
   "id" bigserial PRIMARY KEY,
   "post_id" bigint NOT NULL,
   "body" text NOT NULL,
   "created_at" timestamptz NOT NULL DEFAULT (now())
 );
-
 CREATE TABLE "post_category" (
   "id" bigserial PRIMARY KEY,
   "post_id" bigint NOT NULL,
   "category_id" bigint NOT NULL
 );
-
 CREATE TABLE "category" (
   "id" bigserial PRIMARY KEY,
   "name" varchar NOT NULL
 );
-
 CREATE TABLE "post_favorites" (
   "id" bigserial PRIMARY KEY,
   "post_id" bigint NOT NULL,
   "user_id" bigint NOT NULL
 );
-
 CREATE TABLE "follow" (
   "id" bigserial PRIMARY KEY,
   "following_id" bigint NOT NULL,
   "follower_id" bigint NOT NULL
 );
-
 CREATE TABLE "accounts" (
   "id" bigserial PRIMARY KEY,
   "owner" varchar NOT NULL,
   "balance" bigint NOT NULL,
   "created_at" timestamptz NOT NULL DEFAULT (now())
 );
-
 CREATE TABLE "entries" (
   "id" bigserial PRIMARY KEY,
   "account_id" bigint NOT NULL,
   "amount" bigint NOT NULL,
   "created_at" timestamptz NOT NULL DEFAULT (now())
 );
-
 CREATE TABLE "transfers" (
   "id" bigserial PRIMARY KEY,
   "from_account_id" bigint NOT NULL,
@@ -69,43 +60,36 @@ CREATE TABLE "transfers" (
   "amount" bigint NOT NULL,
   "created_at" timestamptz NOT NULL DEFAULT (now())
 );
-
-ALTER TABLE "posts" ADD FOREIGN KEY ("author") REFERENCES "users" ("username");
-
-ALTER TABLE "comments" ADD FOREIGN KEY ("post_id") REFERENCES "posts" ("id");
-
-ALTER TABLE "post_category" ADD FOREIGN KEY ("post_id") REFERENCES "posts" ("id");
-
-ALTER TABLE "post_category" ADD FOREIGN KEY ("category_id") REFERENCES "category" ("id");
-
-ALTER TABLE "post_favorites" ADD FOREIGN KEY ("post_id") REFERENCES "posts" ("id");
-
-ALTER TABLE "post_favorites" ADD FOREIGN KEY ("user_id") REFERENCES "users" ("id");
-
-ALTER TABLE "follow" ADD FOREIGN KEY ("following_id") REFERENCES "users" ("id");
-
-ALTER TABLE "follow" ADD FOREIGN KEY ("follower_id") REFERENCES "users" ("id");
-
-ALTER TABLE "entries" ADD FOREIGN KEY ("account_id") REFERENCES "accounts" ("id");
-
-ALTER TABLE "transfers" ADD FOREIGN KEY ("from_account_id") REFERENCES "accounts" ("id");
-
-ALTER TABLE "transfers" ADD FOREIGN KEY ("to_account_id") REFERENCES "accounts" ("id");
-
+ALTER TABLE "posts"
+ADD FOREIGN KEY ("author") REFERENCES "users" ("username");
+ALTER TABLE "comments"
+ADD FOREIGN KEY ("post_id") REFERENCES "posts" ("id");
+ALTER TABLE "post_category"
+ADD FOREIGN KEY ("post_id") REFERENCES "posts" ("id");
+ALTER TABLE "post_category"
+ADD FOREIGN KEY ("category_id") REFERENCES "category" ("id");
+ALTER TABLE "post_favorites"
+ADD FOREIGN KEY ("post_id") REFERENCES "posts" ("id");
+ALTER TABLE "post_favorites"
+ADD FOREIGN KEY ("user_id") REFERENCES "users" ("id");
+ALTER TABLE "follow"
+ADD FOREIGN KEY ("following_id") REFERENCES "users" ("id");
+ALTER TABLE "follow"
+ADD FOREIGN KEY ("follower_id") REFERENCES "users" ("id");
+ALTER TABLE "accounts"
+ADD FOREIGN KEY ("owner") REFERENCES "users" ("username");
+ALTER TABLE "entries"
+ADD FOREIGN KEY ("account_id") REFERENCES "accounts" ("id");
+ALTER TABLE "transfers"
+ADD FOREIGN KEY ("from_account_id") REFERENCES "accounts" ("id");
+ALTER TABLE "transfers"
+ADD FOREIGN KEY ("to_account_id") REFERENCES "accounts" ("id");
 CREATE INDEX ON "users" ("username");
-
 CREATE INDEX ON "posts" ("author");
-
 CREATE INDEX ON "accounts" ("owner");
-
 CREATE INDEX ON "entries" ("account_id");
-
 CREATE INDEX ON "transfers" ("from_account_id");
-
 CREATE INDEX ON "transfers" ("to_account_id");
-
 CREATE INDEX ON "transfers" ("from_account_id", "to_account_id");
-
 COMMENT ON COLUMN "entries"."amount" IS 'can be positive or negative';
-
 COMMENT ON COLUMN "transfers"."amount" IS 'must be positive';
