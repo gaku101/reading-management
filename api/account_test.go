f7c8d6af580cb0be892ccb0a8c11c1bc62d7fc33
takehiro <takehiro-r133@hotmail.co.jp>
2021-08-24 20:25:05 +0900

    add login api #25

--- api/account_test.go
+++ api/account_test.go
@@ -26,7 +26,7 @@ func TestGetAccountAPI(t *testing.T) {
 		Return(account, nil)
 
 	// start test server and send request
-	server := NewServer(store)
+	server := newTestServer(t, store)
 	recorder := httptest.NewRecorder()
 
 	url := fmt.Sprintf("/accounts/%d", account.ID)
