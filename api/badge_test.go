9ebc86ff69d4fcdcaf486108b170fd9b56bd303f
takehiro <takehiro-r133@hotmail.co.jp>
2021-11-11 18:57:54 +0900

    add api test for badge #141

---
+++ api/badge_test.go
@@ -0,0 +1,148 @@
+package api
+
+import (
+	"bytes"
+	"database/sql"
+
+	"encoding/json"
+	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+	"time"
+
+	mockdb "github.com/gaku101/my-portfolio/db/mock"
+	db "github.com/gaku101/my-portfolio/db/sqlc"
+	"github.com/gaku101/my-portfolio/token"
+	"github.com/gaku101/my-portfolio/util"
+	"github.com/gin-gonic/gin"
+	"github.com/stretchr/testify/require"
+
+	"github.com/golang/mock/gomock"
+)
+
+func TestCreateBadgeAPI(t *testing.T) {
+	user, _ := randomUser(t)
+	badge := randomBadge()
+
+	testCases := []struct {
+		name          string
+		body          gin.H
+		setupAuth     func(t *testing.T, request *http.Request, tokenMaker token.Maker)
+		buildStubs    func(store *mockdb.MockStore)
+		checkResponse func(recoder *httptest.ResponseRecorder)
+	}{
+		{
+			name: "OK",
+			body: gin.H{
+				"name": badge.Name,
+			},
+			setupAuth: func(t *testing.T, request *http.Request, tokenMaker token.Maker) {
+				addAuthorization(t, request, tokenMaker, authorizationTypeBearer, user.Username, time.Minute)
+			},
+			buildStubs: func(store *mockdb.MockStore) {
+				store.EXPECT().
+					CreateBadge(gomock.Any(), gomock.Eq(badge.Name)).
+					Times(1).
+					Return(badge, nil)
+			},
+			checkResponse: func(recorder *httptest.ResponseRecorder) {
+				require.Equal(t, http.StatusOK, recorder.Code)
+				requireBodyMatchBadge(t, recorder.Body, badge)
+			},
+		},
+		{
+			name: "NoAuthorization",
+			body: gin.H{
+				"name": badge.Name,
+			},
+			setupAuth: func(t *testing.T, request *http.Request, tokenMaker token.Maker) {
+			},
+			buildStubs: func(store *mockdb.MockStore) {
+				store.EXPECT().
+					CreateBadge(gomock.Any(), gomock.Any()).
+					Times(0)
+			},
+			checkResponse: func(recorder *httptest.ResponseRecorder) {
+				require.Equal(t, http.StatusUnauthorized, recorder.Code)
+			},
+		},
+		{
+			name: "InternalError",
+			body: gin.H{
+				"name": badge.Name,
+			},
+			setupAuth: func(t *testing.T, request *http.Request, tokenMaker token.Maker) {
+				addAuthorization(t, request, tokenMaker, authorizationTypeBearer, user.Username, time.Minute)
+			},
+			buildStubs: func(store *mockdb.MockStore) {
+				store.EXPECT().
+					CreateBadge(gomock.Any(), gomock.Any()).
+					Times(1).
+					Return(db.Badge{}, sql.ErrConnDone)
+			},
+			checkResponse: func(recorder *httptest.ResponseRecorder) {
+				require.Equal(t, http.StatusInternalServerError, recorder.Code)
+			},
+		},
+		{
+			name: "InvalidName",
+			body: gin.H{
+				"name": 0,
+			},
+			setupAuth: func(t *testing.T, request *http.Request, tokenMaker token.Maker) {
+				addAuthorization(t, request, tokenMaker, authorizationTypeBearer, user.Username, time.Minute)
+			},
+			buildStubs: func(store *mockdb.MockStore) {
+				store.EXPECT().
+					CreateBadge(gomock.Any(), gomock.Any()).
+					Times(0)
+			},
+			checkResponse: func(recorder *httptest.ResponseRecorder) {
+				require.Equal(t, http.StatusBadRequest, recorder.Code)
+			},
+		},
+	}
+
+	for i := range testCases {
+		tc := testCases[i]
+
+		t.Run(tc.name, func(t *testing.T) {
+			ctrl := gomock.NewController(t)
+			defer ctrl.Finish()
+
+			store := mockdb.NewMockStore(ctrl)
+			tc.buildStubs(store)
+
+			server := newTestServer(t, store)
+			recorder := httptest.NewRecorder()
+
+			// Marshal body data to JSON
+			data, err := json.Marshal(tc.body)
+			require.NoError(t, err)
+
+			url := "/badge"
+			request, err := http.NewRequest(http.MethodPost, url, bytes.NewReader(data))
+			require.NoError(t, err)
+
+			tc.setupAuth(t, request, server.tokenMaker)
+			server.router.ServeHTTP(recorder, request)
+			tc.checkResponse(recorder)
+		})
+	}
+}
+func randomBadge() db.Badge {
+	return db.Badge{
+		ID:   util.RandomInt(1, 1000),
+		Name: util.RandomString(6),
+	}
+}
+func requireBodyMatchBadge(t *testing.T, body *bytes.Buffer, badge db.Badge) {
+	data, err := ioutil.ReadAll(body)
+	require.NoError(t, err)
+
+	var gotBadge db.Badge
+	err = json.Unmarshal(data, &gotBadge)
+	require.NoError(t, err)
+	require.Equal(t, badge, gotBadge)
+}
