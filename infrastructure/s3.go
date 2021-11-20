4e7644c406e3a0dcbe2798513dd083c5ca0f93e3
takehiro <takehiro-r133@hotmail.co.jp>
2021-11-20 19:43:22 +0900

    change s3 config

--- infrastructure/s3.go
+++ infrastructure/s3.go
@@ -7,6 +7,7 @@ import (
 	"strings"
 
 	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/credentials"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/s3"
 	"github.com/aws/aws-sdk-go/service/s3/s3manager"
@@ -27,8 +28,11 @@ func NewAwsS3() *AwsS3 {
 	config := NewConfig()
 	// s3manager.Uploader を初期化
 	sess, err := session.NewSessionWithOptions(session.Options{
-		Config:  aws.Config{Region: aws.String(config.Aws.S3.Region)},
-		Profile: "github",
+		Config: aws.Config{Credentials: credentials.NewStaticCredentialsFromCreds(credentials.Value{
+			AccessKeyID:     config.Aws.S3.AccessKeyID,
+			SecretAccessKey: config.Aws.S3.SecretAccessKey,
+		}),
+			Region: aws.String(config.Aws.S3.Region)},
 	})
 	if err != nil {
 		panic(err)
